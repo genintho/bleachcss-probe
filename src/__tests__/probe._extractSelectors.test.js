a43b1a5743757ad7d287297c26ecd6e27c4502fd
Thomas Genin <xt6.thomas.genin@gmail.com>
2017-05-11 21:46:02 -0700

    Handle keyframe rules correctly

--- src/__tests__/probe._extractSelectors.test.js
+++ src/__tests__/probe._extractSelectors.test.js
@@ -96,14 +96,20 @@ describe("Probe Selector Extraction", () => {
                 return postcss().process(cssSrc, {}).then(function postCSSProcessResult(postCSSResult) {
                     const postCssSelectors = new Set();
                     postCSSResult.root.walkRules(function(rule) {
-                        const splits = rule.selector.split(",");
-                        splits.forEach(item => {
+                        if (rule.parent && rule.parent.name && rule.parent.name.indexOf("keyframes") !== -1) {
+                            return;
+                        }
+                        rule.selectors.forEach(item => {
                             var splits = item.split(":");
                             var selector = item;
                             if (splits[splits.length - 1] !== "first-child") {
                                 selector = splits[0];
                             }
                             selector = selector.trim();
+                            if (selector === "40%") {
+                                // console.log(rule.parent.type);
+                                console.log(rule.parent.name);
+                            }
                             postCssSelectors.add(selector);
                         });
                     });

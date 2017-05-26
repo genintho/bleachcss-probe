b5b68ccc65afb3d09ec40245698b811f829c019c
Thomas Genin <xt6.thomas.genin@gmail.com>
2017-05-25 19:13:33 -0700

    Handle more cases of parent selector detection

--- src/__tests__/probe._findParentSelector.test.js
+++ src/__tests__/probe._findParentSelector.test.js
@@ -1,24 +1,47 @@
 var Probe = require("../probe");
 
 var testInputs = {
-    ".a": null,
-    ".a .b": ".a",
-    ".a.b": null,
-    ".a.b .c": ".a.b",
-    ".a .b.c": ".a",
-    ".a > .b": ".a",
-    ".a ~ .b": ".a",
-    ".a + .b": ".a"
+    ".a": [null],
+    ".a.b": [null],
+    ".a.b.c": [null],
+
+    ".a .b": [".a"],
+    ".a .b .c": [".a .b", ".a"],
+    ".a.b .c": [".a.b"],
+    ".a .b.c": [".a"],
+
+    ".a > .b": [".a"],
+    ".a > .b > .c": [".a > .b", ".a"],
+
+    ".a ~ .b": [".a"],
+    ".a ~ .b~.c": [".a ~ .b", ".a"],
+
+    ".a + .b": [".a"],
+    ".a + .b + .c": [".a + .b", ".a"],
+    ".a + .b ~ .c": [".a + .b", ".a"],
+
+    ".a+.b ~ .c.d > .e": [".a+.b ~ .c.d", ".a+.b", ".a"]
 };
 
 describe("PostCSS _findParentSelector", () => {
-    Object.keys(testInputs).forEach((selector, idx) => {
-        const expectedParentSelector = testInputs[selector];
-        test("_findParentSelector " + selector, () => {
-            var p = new Probe();
-            p._addSelector = jest.fn();
-            const ret = p._findParentSelector(selector);
-            expect(ret).toBe(expectedParentSelector);
+    describe("extraction test", () => {
+        Object.keys(testInputs).forEach((selector, idx) => {
+            const expectedParentSelector = testInputs[selector][0];
+            test(selector, () => {
+                var p = new Probe();
+                var spy = jest.spyOn(p, "_findParentSelector");
+                const ret = p._findParentSelector(selector);
+                expect(ret).toBe(expectedParentSelector);
+                if (expectedParentSelector !== null) {
+                    // Test that we have detected all the parents
+                    expect(spy).toHaveBeenCalledTimes(testInputs[selector].length + 1);
+
+                    // Test that we find them correctly
+                    testInputs[selector].forEach(inn => {
+                        expect(spy).toHaveBeenCalledWith(inn);
+                    });
+                }
+            });
         });
     });
 });
